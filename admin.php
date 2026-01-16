39c5ec6d9664baeb96857e5dcf97792f5041f500
kiael-ms <lkia33475@gmail.com>
2026-01-16 21:11:10 +0700

    menambahkan dropdown profile pada admin

--- admin.php
+++ admin.php
@@ -58,7 +58,8 @@ if (!isset($_SESSION['username'])) {
 						<?= $_SESSION['username']?>
 					</a>
 					<ul class="dropdown-menu">
-						<li><a class="dropdown-item" href="logout.php">Logout</a></li> 
+						<li><a class="dropdown-item" href="profile.php">Profile</a></li>
+						<li><a class="dropdown-item" href="logout.php">Logout</a></li>  
 					</ul>
 				</li> 
 			</ul>
