6a2b6a0937c02dbddcdf40fccbd94a3919e488ee
kiael-ms <lkia33475@gmail.com>
2026-01-16 23:10:22 +0700

    file profile dan profile update

--- dashboard.php
+++ dashboard.php
@@ -12,7 +12,35 @@ $hasil1 = $conn->query($sql1);
 //menghitung jumlah baris data gallery
 $jumlah_gallery = $hasil1->num_rows; 
 
+$username = $_SESSION['username'];
+$qUser = mysqli_query(
+    $conn,
+    "SELECT username, foto FROM user WHERE username='$username'"
+);
+$user = mysqli_fetch_assoc($qUser);
+
+
 ?>
+<div class="container text-center mt-5">
+
+    <p class="mt-4">Selamat Datang,</p>
+    <h3 class="text-danger fw-bold">
+        <?= $user['username']; ?>
+    </h3>
+
+ <!-- FOTO PROFIL -->
+    <div class="my-4">
+        <?php if (!empty($user['foto'])) { ?>
+            <img src="img/<?= $user['foto']; ?>"
+                class="rounded-circle shadow"
+                style="width:180px;height:180px;object-fit:cover;">
+        <?php } else { ?>
+            <img src="img/default.png"
+                class="rounded-circle shadow"
+                style="width:180px;height:180px;">
+        <?php } ?>
+    </div>
+</div>
 <div class="row row-cols-1 row-cols-md-4 g-4 justify-content-center pt-4">
     <div class="col">
         <div class="card border border-danger mb-3 shadow" style="max-width: 18rem;">
